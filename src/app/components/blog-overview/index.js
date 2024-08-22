c0f559b5a98fd495ea596720129b2e0983c21edb
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-08-22 22:56:44 +0530

    index.js

--- src/app/components/blog-overview/index.js
+++ src/app/components/blog-overview/index.js
@@ -1,50 +1,48 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
 import { useState } from "react";
+import AddNewBlog from "../add-new-blog";
+
+const initialBlogFormData = {
+  title: "",
+  description: "",
+};
 
 function BlogOverview() {
   const [openBlogDialog, setOpenBlogDialog] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [blogFormData, setBlogFormData] = useState(initialBlogFormData);
+
+  console.log(blogFormData);
+
+  async function handleSaveBlogData() {
+    try {
+      const apiResponse = await fetch("/api/add-blog", {
+        method: "POST",
+        body: JSON.stringify(blogFormData),
+      });
+
+      const result = await apiResponse.json();
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setBlogFormData(initialBlogFormData);
+    }
+  }
 
   return (
     <div className="min-h-screen flex flex-col gap-10  bg-gradient-to-r from-purple-300 to to-blue-300 p-6">
-      <div>
-        <Button onClick={() => setOpenBlogDialog(true)}>Add new Blog</Button>
-      </div>
+      <AddNewBlog
+        openBlogDialog={openBlogDialog}
+        setOpenBlogDialog={setOpenBlogDialog}
+        loading={loading}
+        setLoading={setLoading}
+        blogFormData={blogFormData}
+        setBlogFormData={setBlogFormData}
+        handleSaveBlogData={handleSaveBlogData}
+      />
       <div>Blog List Section</div>
-      <Dialog open={openBlogDialog} onOpenChange={setOpenBlogDialog}>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Add New Blog</DialogTitle>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Title
-              </Label>
-              <Input id="title" className="col-span-3" />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" className="text-right">
-                Description
-              </Label>
-              <Input id="description" className="col-span-3" />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="button">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </div>
   );
 }
