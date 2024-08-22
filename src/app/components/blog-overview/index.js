b1aa937266d7e05c92ce8b62f592dd4cec3696a5
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-08-22 20:10:57 +0530

    index.js

--- src/app/components/blog-overview/index.js
+++ src/app/components/blog-overview/index.js
@@ -1,13 +1,50 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 function BlogOverview() {
+  const [openBlogDialog, setOpenBlogDialog] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col gap-10  bg-gradient-to-r from-purple-300 to to-blue-300 p-6">
       <div>
-        <Button>Add new Blog</Button>
+        <Button onClick={() => setOpenBlogDialog(true)}>Add new Blog</Button>
       </div>
       <div>Blog List Section</div>
+      <Dialog open={openBlogDialog} onOpenChange={setOpenBlogDialog}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add New Blog</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Title
+              </Label>
+              <Input id="title" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Description
+              </Label>
+              <Input id="description" className="col-span-3" />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="button">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
