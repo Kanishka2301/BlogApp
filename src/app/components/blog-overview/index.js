133249922ba10f17ff5b23de285e1413352121e1
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-08-22 19:51:45 +0530

    index.js

--- src/app/components/blog-overview/index.js
+++ src/app/components/blog-overview/index.js
@@ -2,7 +2,10 @@
 
 function BlogOverview() {
   return (
-    <div className="min-h-screen flex-col gap-10  bg-gradient-to-r from-purple-300 to to-blue-300 p-6"></div>
+    <div className="min-h-screen flex flex-col gap-10  bg-gradient-to-r from-purple-300 to to-blue-300 p-6">
+      <div>Add new Blog Section</div>
+      <div>Blog List Section</div>
+    </div>
   );
 }
 
