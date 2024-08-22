8238fcaeb32a9753f679b3543e93e20076f02d2b
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-08-22 20:00:17 +0530

    index.js

--- src/app/components/blog-overview/index.js
+++ src/app/components/blog-overview/index.js
@@ -1,9 +1,12 @@
 "use client";
+import { Button } from "@/components/ui/button";
 
 function BlogOverview() {
   return (
     <div className="min-h-screen flex flex-col gap-10  bg-gradient-to-r from-purple-300 to to-blue-300 p-6">
-      <div>Add new Blog Section</div>
+      <div>
+        <Button>Add new Blog</Button>
+      </div>
       <div>Blog List Section</div>
     </div>
   );
