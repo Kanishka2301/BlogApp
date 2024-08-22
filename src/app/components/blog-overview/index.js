b0645a14d05ae2e03b75c87d09430f2535dae978
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-08-22 19:49:37 +0530

    index.js

--- src/app/components/blog-overview/index.js
+++ src/app/components/blog-overview/index.js
@@ -1,7 +1,9 @@
 "use client";
 
 function BlogOverview() {
-  return <div>Blog overview Page</div>;
+  return (
+    <div className="min-h-screen flex-col gap-10  bg-gradient-to-r from-purple-300 to to-blue-300 p-6"></div>
+  );
 }
 
 export default BlogOverview;
